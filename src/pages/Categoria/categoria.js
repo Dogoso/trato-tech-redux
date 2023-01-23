88d22a55206ba4c951924de2fdf19e6fe29b9743
Silas Lago <juanfsa@live.com>
2023-01-23 19:19:47 -0300

    Itens added

--- src/pages/Categoria/categoria.js
+++ src/pages/Categoria/categoria.js
@@ -1,20 +1,30 @@
 import Header from "components/Header/header";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import Item from "./Components/item";
+import styles from "./categoria.module.scss";
 
 function Categoria() {
 
   const params = useParams();
-  const categoria = useSelector(state => state.categorias.find(categoria => categoria.id === params.idCategoria));
+  const { categoria, itens } = useSelector(state => ({
+    categoria: state.categorias.find(categoria => categoria.id === params.idCategoria),
+    itens: state.itens.filter(item => item.categoria === params.idCategoria)
+  }));
 
   return (
     <section>
       {params.idCategoria ? (
-        <Header
-          titulo={categoria.nome}
-          descricao={categoria.descricao}
-          imagem={categoria.header}
-        />
+        <>
+          <Header
+            titulo={categoria.nome}
+            descricao={categoria.descricao}
+            imagem={categoria.header}
+          />
+          <section className={styles.itens}>
+            {itens?.map(item => <Item key={item.id} {...item} />)}
+          </section>
+        </>
       ) : "Error 404!"}
     </section>
   )
